6f9a8b4647886a8cfc71a3084c75d26c5a86293c
Elvina Fattakhova <elvinaf1996@gmail.com>
2022-08-08 19:53:01 +0300

    changed

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -15,13 +15,11 @@ public abstract class BaseTest {
 
     @Before
     public void setUp() {
+        WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
+        Configuration.driverManagerEnabled = true;
+        Configuration.headless = false;
         Configuration.browserSize = "1920x1080";
-        Configuration.remote = "http://localhost:4444/wd/hub";
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
     }
 
     @After
